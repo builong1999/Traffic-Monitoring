c0315f034ae17caba0b4b003a8777b8d5b46e83d
lhduong1999 <lhduong1999@gmail.com>
2021-01-27 02:46:17 +0700

    label select

---
+++ prepare_label _segmentation.py
@@ -0,0 +1,14 @@
+from operator import truediv
+from PreStage.Label.make_label_segmentation import MakeLabelSegmentation
+import glob
+import os
+imgPath = 'E:/test' #Recommended: ROI this directory in advance!
+resultPath = 'E:/res_PhamVanDong-Duong20(huongdiThuDuc)'
+make_label = MakeLabelSegmentation(src_path=imgPath,result_path=resultPath)
+make_label.start()
+#Left mouse button: select vehicle 
+#SHIFT+left mouse button: select ROI
+#SHIFT+left mouse button: select Street
+#'r'/SPACE: reset all         'w': change vision
+#'p': prev image              'n': next image
+#'s'/ENTER: save label        'q'/ESC: exit
