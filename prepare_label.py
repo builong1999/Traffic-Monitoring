adeb0c94435bbfce7d8b647007f3d1baaa984f2d
lhduong1999 <lhduong1999@gmail.com>
2021-01-27 01:32:22 +0700

    label

--- prepare_label.py
+++ prepare_label.py
@@ -1,8 +1,8 @@
 from PreStage.Label.make_label import MakeLabel
 import glob
 import os
-path = 'E:/PhamVanDong-Duong20(huongdiSanbay)'
-make_label = MakeLabel(direc = path,unlabeled = True, scale = 1.5)
+path = 'E:/PhamVanDong-Duong20(huongdiThuDuc)'
+make_label = MakeLabel(direc = path,unlabeled = False, scale = 1.5)
 make_label.start()
 #click mouse and enter is make label
 #drag mouse and enter is delete label
--- prepare_roi.py
+++ prepare_roi.py
@@ -1,6 +1,6 @@
 from os import replace
 from PreStage.ROI.make_roi import selectROI
-path = 'E:/PhamVanDong-Duong20(huongdiThuDuc)/'
+path = 'E:/HoangVanThu-UtTich/'
 selROI = selectROI(direc = path, replaceImg = False) 
 #replace img with ROI if relapceImg = True else create folder RoiImage and save image with ROI
 selROI.createROI() #create new ROI img
